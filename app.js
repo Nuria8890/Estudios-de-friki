705098525b8addc58866c3d4db45cc125401c106
César M. Cristóbal <cesar@callepuzzle.com>
2022-06-15 09:26:36 +0000

    pruebas

--- app.js
+++ app.js
@@ -3,7 +3,9 @@ const app = express()
 const port = 3000
 
 app.use(express.static('public'))
-
+app.use(function (req, res, next) {
+  res.set('Cache-control', 'no-cache, no-store, must-revalidate, max-age=0')
+})
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
